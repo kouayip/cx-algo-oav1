229b059b6668dea64a928f04dfad044f9e220987
Yves Kouayip (L'amchard) <kamwayves37@gmail.com>
2020-10-16 11:05:57 +0200

    update function howManyCamelCase

---
+++ main.js
@@ -0,0 +1,4 @@
+
+const howManyCamelCase = require('./step01')
+const numberOfCamelCase = howManyCamelCase('yveSamsPPpp')
+console.log(numberOfCamelCase);
--- step01.js
+++ step01.js
@@ -1,16 +1,13 @@
 
 
-function howManyCamelCase(string) {
+module.exports = function (string) {
     let count = 1
     for (let index = 1; index < string.length - 1; index++) {
         let currentChar = string[index]
         let nextChar = string[index + 1]
         if(currentChar === currentChar.toUpperCase() && nextChar === nextChar.toLowerCase()){
-            console.log(currentChar);
             count++
         }
     }
     return count
 }
-
-console.log(howManyCamelCase('YvesKams'));
