cc2511095082e5715fb39e526b433e42b03f0c2e
Yves Kouayip (L'amchard) <kamwayves37@gmail.com>
2020-10-16 12:20:38 +0200

    function IsPangrams is done

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
-const howManyCamelCase = require('./step01')
-const numberOfCamelCase = howManyCamelCase('yveSamsPPpp')
-console.log(numberOfCamelCase);
+// const howManyCamelCase = require('./step01')
+// const numberOfCamelCase = howManyCamelCase('yveSamsPPpp')
+// console.log(numberOfCamelCase);
 
 const IsAPangrams = require('./step02')
 console.log(IsAPangrams("The quick brown fox jumps over the lazy dog"));
--- step02.js
+++ step02.js
@@ -1,5 +1,16 @@
 
 
-module.exports = function(){
-    
+module.exports = function(str){
+    const validator = {}
+    let score = 0;
+    for (let index = 0; index < str.length; index++) {
+        const char = str[index].toLowerCase()
+        const charCode = char.charCodeAt(0)
+        if((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)){
+            validator[charCode] = char;
+
+        }
+    }
+
+    return Object.keys(validator).length === 26
 }
