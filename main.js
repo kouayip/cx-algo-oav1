5345e96552b4ed02c976344453565343bdb2067d
Yves Kouayip (L'amchard) <kamwayves37@gmail.com>
2020-10-16 11:09:57 +0200

    init test function isAPangrams

--- main.js
+++ main.js
@@ -2,3 +2,7 @@
 const howManyCamelCase = require('./step01')
 const numberOfCamelCase = howManyCamelCase('yveSamsPPpp')
 console.log(numberOfCamelCase);
+
+const IsAPangrams = require('./step02')
+console.log(IsAPangrams("The quick brown fox jumps over the lazy dog"));
+console.log(IsAPangrams("Hello Efrei"));
